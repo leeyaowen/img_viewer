ac6b12a062e4fdd479b23aa7d7c9b4afbe9bc2ea
leeyaowen <jcjh30101@gmail.com>
2021-04-27 04:58:26 +0800

    add xy

--- seg_check.py
+++ seg_check.py
@@ -19,15 +19,26 @@ class SegPanel(wx.Panel):
         self.SetBackgroundColour((240, 240, 240))
         self.currentDirectory = os.getcwd()
 
-        dirdlgBtn = wx.Button(self, label="Select Image", pos=(10, 20), size=(150, 30))
-        dirdlgBtn.Bind(wx.EVT_BUTTON, self.ondir)
+        self.dirdlgBtn = wx.Button(self, label='Select Image', pos=(10, 20), size=(150, 30))
+        self.dirdlgBtn.Bind(wx.EVT_BUTTON, self.ondir)
         self.PhotoMaxSize = 500
 
         img = wx.Image(500, 500)
         self.imageCtrl = wx.StaticBitmap(self, wx.ID_ANY,
                                          wx.Bitmap(img),
                                          pos=(10, 70))
+        self.imageCtrl.Bind(wx.EVT_MOUSE_EVENTS, self.movemouse)
+        
+        self.setdpiAwarenessBtn = wx.Button(self, label='Set DPI Awareness', pos=(180, 20), size=(150, 30))
+        self.setdpiAwarenessBtn.Bind(wx.EVT_BUTTON, self.setdpiAwareness)
 
+        self.labelX = wx.StaticText(self, label='', pos=(10, 580), size=(50, -1), style=wx.ALIGN_CENTRE)
+        self.labelY = wx.StaticText(self, label='', pos=(65, 580), size=(50, -1), style=wx.ALIGN_CENTRE)
+        self.labelX.SetBackgroundColour('white')
+        self.labelY.SetBackgroundColour('white')
+        self.Bind(wx.EVT_MOTION, self.movemouse)
+
+        
     def ondir(self, event):
         self.dlg = wx.FileDialog(self, 'Choose a file:',
                             wildcard='pictures (*.jpg,*.png)|*.jpg;*.png',
@@ -58,10 +69,17 @@ class SegPanel(wx.Panel):
         self.imageCtrl.SetBitmap(wx.Bitmap(img))
         self.Refresh()
 
+    def setdpiAwareness(self, event):
+        ctypes.windll.shcore.SetProcessDpiAwareness(2)
+
+    def movemouse(self, event):
+        x, y = self.ScreenToClient(wx.GetMousePosition())
+        self.labelX.SetLabel(str(x-10))
+        self.labelY.SetLabel(str(y-70))
+
 
 if __name__ == '__main__':
     app = wx.App()
     frame = SegWindow(None)
     frame.Show(True)
-    ctypes.windll.shcore.SetProcessDpiAwareness(2)
     app.MainLoop()
